0878c3b0c47e39912300e621acd5d2bec101fae2
Oliver Matthews <o.matthews@rocketmail.com>
2019-05-01 15:50:28 +0100

    wrote unit tests for dogEntity class

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,4 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Doginator\\DBconnector' => $baseDir . '/src/DBconnector.php',
+    'Doginator\\Entities\\DogEntity' => $baseDir . '/src/Entities/DogEntity.php',
 );
