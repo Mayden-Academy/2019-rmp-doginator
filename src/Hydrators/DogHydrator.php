f93b3b2e2876eeed0f71c4595dfd328a34e34ba0
Weylon Neil Bulloch <panatha_4ever@hotmail.com>
2019-05-02 10:52:20 +0100

    Hydrating dog entity with images

--- src/Hydrators/DogHydrator.php
+++ src/Hydrators/DogHydrator.php
@@ -2,20 +2,46 @@
 
 namespace Doginator\Hydrators;
 
-//use Doginator\Entities\DogEntity;
+use Doginator\Entities\DogEntity;
 
 class DogHydrator
 {
 
-   public function getDogEntity($connection, $breed_id)
+   public static function getDogEntity($dbConnection, $breed_id)
    {
-        $sql = 'SELECT `name`, `id` FROM `breed` WHERE `id` = (:breed_id);';
 
-        $statement = $connection->prepare($sql);
+        $sql = 'SELECT `breed` . `id` , `breed` . `name`, `image`. `url` 
+                FROM `breed` LEFT JOIN `image` ON `image` . `breed_id` = `breed` . `id` 
+                WHERE `breed`.`id` = (:breed_id);';
+
+        $statement = $dbConnection->prepare($sql);
         $statement->bindparam(':breed_id', $breed_id);
         $statement->execute();
-        $results = $statement->fetch();
-        var_dump($results);
-//        $dog = new DogEntity()
+        $results = $statement->fetchAll();
+        $dogImgsArray = [];
+        $name = $results [0]['name'];
+        foreach ($results as $breed) {
+            $url = $breed['url'];
+            array_push($dogImgsArray, $url);
+        }
+        $dog = new DogEntity($breed_id, $name, $dogImgsArray);
+        return $dog;
+
+   }
+
+   public static function getDogEntities($dbConnection)
+   {
+       $sql = 'SELECT `name`, `id` FROM `breed`;';
+       $statement = $dbConnection->prepare($sql);
+       $statement->execute();
+       $results = $statement->fetchAll();
+       $dogArray = [];
+       foreach($results as $breed) {
+           $id = $breed['id'];
+           $name = $breed['name'];
+           $dog = new DogEntity($id, $name);
+           array_push($dogArray, $dog);
+       }
+       return $dogArray;
    }
 }
