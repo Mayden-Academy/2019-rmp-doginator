9e8b5e1ec35e1723e5f5adc21a67b129fcaffeb5
Weylon Neil Bulloch <panatha_4ever@hotmail.com>
2019-05-02 14:17:21 +0100

    fixed concatenation

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ $allDogEntities = DogHydrator::getDogEntities($dbConnection);
                 foreach ($allDogEntities as $dogEntity) {
                     $id = $dogEntity->getBreedId();
                     $breed = $dogEntity->getBreed();
-                    echo '<a class="dropdown-item" href="http://192.168.20.20/2019-rmp-doginator/gallery.php?id=' . $id . '"' . '>' . $breed . '</a>';
+                    echo '<a class="dropdown-item" href="http://192.168.20.20/2019-rmp-doginator/gallery.php?id=' . $id . '">' . $breed . '</a>';
                 }
                 ?>
             </div>
