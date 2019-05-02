80ef74dad07be60ab491eec9ae3b0581fce9af06
Alessandro Corra <alecorra1980@gmail.com>
2019-05-02 15:54:13 +0100

    css fixed

--- gallery.php
+++ gallery.php
@@ -17,25 +17,29 @@ $allDogEntities = DogHydrator::getDogEntities($dbConnection);
 <head>
     <meta charset="UTF-8">
     <title>DOGINATOR || Gallery</title>
-    <link rel="stylesheet" href="CSS/normalize.css" type="text/css">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="CSS/normalize.css" type="text/css">
     <link rel="stylesheet" href="CSS/style.css" type="text/css">
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <div class="content-container">
+    <div class="container">
         <div class="row">
-            <img src="Assets/Images/logo.svg" class="logo left">
-            <div class="col header">
+            <div class="col-3">
+                <img src="Assets/Images/logo.svg" class="logo">
+            </div>
+
+            <div class="col-6 header">
                 <img src="Assets/Images/bone.svg" class="bone gallery-bone">
                 <h1 class="gallery-title">DOGINATOR</h1>
             </div>
-            <img src="Assets/Images/logo.svg" class="logo right">
+
+            <div class="col-3">
+                <img src="Assets/Images/logo.svg" class="logo">
+            </div>
         </div>
 
         <div class="dropdown show gallery-dropdown">
@@ -61,7 +65,7 @@ $allDogEntities = DogHydrator::getDogEntities($dbConnection);
             </div>
         </div>
 
-        <div class="container">
+<!--        <div class="container">-->
             <div class="row">
                 <?php
                 if (empty($dog->getImages())) {
@@ -74,7 +78,7 @@ $allDogEntities = DogHydrator::getDogEntities($dbConnection);
                     }
                 }?>
             </div>
-        </div>
+<!--        </div>-->
     </div>
 </body>
 </html>
